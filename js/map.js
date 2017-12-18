c3b9cb13c4b40c38123f329a82d9271356b42dbc
Julia Buzina <buzinay@mail.ru>
2017-12-18 10:48:44 +0300

    Добавила обработку перемещения главного пина

--- js/map.js
+++ js/map.js
@@ -28,13 +28,90 @@
     closePopup();
   };
 
-  // После того, как на блоке map__pin--main произойдет событие mouseup, форма и карта должны активироваться:
+  // передвижение главного пина
   var mapPinMain = map.querySelector('.map__pin--main');
+  var addressField = document.querySelector('#address');
+  var addressCoordinate = {
+    x: mapPinMain.offsetLeft + mapPinMain.offsetWidth / 2,
+    y: mapPinMain.offsetTop + mapPinMain.offsetHeight
+  };
+  addressField.value = 'x: ' + addressCoordinate.x + ' ' + 'y: ' + addressCoordinate.y;
+
+  var coordinateRange = window.data.getCoordinateRange();
+  var minX = coordinateRange.minX;
+  var maxX = coordinateRange.maxX;
+  var minY = coordinateRange.minY;
+  var maxY = coordinateRange.maxY;
+
+  window.console.log('ширина пина ' + mapPinMain.offsetWidth);
+  window.console.log('высота пина ' + mapPinMain.offsetHeight);
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var mapPinMainTop = mapPinMain.offsetTop - shift.y;
+      var mapPinMainLeft = mapPinMain.offsetLeft - shift.x;
+
+      if (mapPinMainTop < minY) {
+        mapPinMainTop = minY;
+      }
+
+      if (mapPinMainTop > maxY) {
+        mapPinMainTop = maxY;
+      }
+
+      if (mapPinMainLeft < minX) {
+        mapPinMainLeft = minX;
+      }
+
+      if (mapPinMainLeft > maxX) {
+        mapPinMainLeft = maxX;
+      }
+
+      mapPinMain.style.top = mapPinMainTop + 'px';
+      mapPinMain.style.left = mapPinMainLeft + 'px';
+
+      addressCoordinate.x = mapPinMainLeft + mapPinMain.offsetWidth / 2;
+      addressCoordinate.y = mapPinMainTop + mapPinMain.offsetHeight;
+
+
+      addressField.value = 'x: ' + addressCoordinate.x + ' ' + 'y: ' + addressCoordinate.y;
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  // После того, как на блоке map__pin--main произойдет событие mouseup, форма и карта должны активироваться:
   mapPinMain.addEventListener('mouseup', function () {
     map.classList.remove('map--faded'); // У карты убрать класс map--faded
     window.utils.switchHidden(pins);
     // У формы убрать класс notice__form--disabled и сделать все поля формы активными
-    window.form.disabledForm();
+    window.form.disabledForm(false);
   });
 
   // При нажатии на любой из элементов .map__pin ему должен добавляться класс map__pin--active и должен показываться элемент .popup
